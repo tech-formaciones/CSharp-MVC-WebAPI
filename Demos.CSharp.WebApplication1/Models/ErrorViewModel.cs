6a6146e1653cd5461c481e5df7beded4619f3255
Borja Cabeza Rozas <borjacabezarozas@gmail.com>
2024-11-20 16:36:11 +0100

    Commit  Inicial

---
+++ Demos.CSharp.WebApplication1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Demos.CSharp.WebApplication1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
